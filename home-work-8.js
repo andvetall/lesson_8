c0c3ef5ade5530548266b1d8ecffc26e094a1e42
Vitalii Andriichuk <45999285+andvetall@users.noreply.github.com>
2019-03-24 16:57:57 +0200

    Add files via upload

---
+++ home-work-8.js
@@ -0,0 +1,15 @@
+var clock = document.body.appendChild(
+    document.createElement('h3')
+)
+
+
+for(var x = 0; x < 100; x++){
+    (function(arg){
+        return function(){
+            setTimeout(
+                () => clock.innerText = new Date().toLocaleString().split(', ')[1],
+                1000 * arg
+            )
+        }
+    })(x)()
+}
